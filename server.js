45764330f51681d149cfe9c4f975685e23d62c9b
jun2723 <166277636+jun2723@users.noreply.github.com>
2024-04-30 13:40:16 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const port = 3002;
 // 정적 파일을 제공하기 위해 Express에게 'public' 디렉토리를 사용하도록 지시합니다.
 app.use(express.static(__dirname));
 
-// 인덱스 페이지로 들어오는 요청에 대한 처리
-app.get('/index', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
 // 로그인 페이지로 들어오는 요청에 대한 처리
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/login.html');
@@ -30,5 +25,5 @@ app.get('/view', (req, res) => {
 
 // 서버를 지정한 포트로 실행합니다.
 app.listen(port, () => {
-    console.log(`서버가 http://localhost:${port}/index 에서 실행 중입니다.`);
+    console.log(`서버가 http://localhost:${port}/login 에서 실행 중입니다.`);
 });
