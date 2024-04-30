49d112d4708cb7b17b72bc583b0873c2357d3187
jun2723 <166277636+jun2723@users.noreply.github.com>
2024-04-30 13:30:14 +0900

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+// login.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var studentIdInput = document.getElementById('student_id');
+    var passwordInput = document.getElementById('password');
+
+    // 입력 필드에 포커스를 받으면 placeholder를 삭제
+    studentIdInput.addEventListener('focus', function() {
+        studentIdInput.placeholder = '';
+    });
+
+    passwordInput.addEventListener('focus', function() {
+        passwordInput.placeholder = '';
+    });
+
+    // 입력 필드에서 포커스를 잃으면 placeholder를 다시 설정
+    studentIdInput.addEventListener('blur', function() {
+        studentIdInput.placeholder = '학  번';
+    });
+
+    passwordInput.addEventListener('blur', function() {
+        passwordInput.placeholder = '비밀번호';
+    });
+});
---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 3002;
+
+// 정적 파일을 제공하기 위해 Express에게 'public' 디렉토리를 사용하도록 지시합니다.
+app.use(express.static(__dirname));
+
+// 인덱스 페이지로 들어오는 요청에 대한 처리
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// 로그인 페이지로 들어오는 요청에 대한 처리
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+// 작성 페이지로 들어오는 요청에 대한 처리
+app.get('/test2', (req, res) => {
+    res.sendFile(__dirname + '/test2.html');
+});
+
+
+// 작성 페이지로 들어오는 요청에 대한 처리
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/view.html');
+});
+
+// 서버를 지정한 포트로 실행합니다.
+app.listen(port, () => {
+    console.log(`서버가 http://localhost:${port}/index 에서 실행 중입니다.`);
+});
